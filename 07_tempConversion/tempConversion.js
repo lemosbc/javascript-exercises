f550aef5c42b0888e175b160578b8d3fd9bfceaa
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2022-12-29 23:55:47 +0000

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  let roundCelsius = celsius.toFixed(1);
+  return +roundCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  let roundFahrenheit = fahrenheit.toFixed(1);
+  return +roundFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
