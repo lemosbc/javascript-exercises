3dceafe302a896103a314b402bee749accba1755
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2022-12-29 23:31:04 +0000

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (i, j) {
+  let sum = 0;
+  if (typeof i != "number" || typeof j != "number") {
+    return "ERROR";
+  } else if (i < 0 || j < 0) {
+    return "ERROR";
+  } else if (i > j) {
+    for (k = j; k <= i; k++) {
+      sum = sum + k;
+    }
+  } else if (i < j) {
+    for (m = i; m <= j; m++) {
+      sum = sum + m;
+    }
+  } else {
+    return "ERROR";
+  }
+  return sum;
 };
 
 // Do not edit below this line
