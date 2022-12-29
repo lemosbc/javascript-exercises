367378292849ee386c6dea916b99a20a1fd15a21
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2022-12-29 23:38:48 +0000

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeapYear;
+  if (year % 400 == 0) {
+    isLeapYear = true;
+  } else if (year % 4 == 0 && !(year % 100 == 0)) {
+    isLeapYear = true;
+  } else {
+    isLeapYear = false;
+  }
 
+  return isLeapYear;
 };
 
 // Do not edit below this line
